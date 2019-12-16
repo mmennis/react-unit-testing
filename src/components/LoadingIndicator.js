24fc1b73df77274a0a682c4ac70fbb97c1700d67
Michael Mennis <mmennis@gmail.com>
2019-12-16 19:56:46 +0000

    make isLoading a static propType

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class LoadingIndicator extends React.Component {
+
+    static propTypes = {
+        isLoading: PropTypes.bool.isRequired
+    };
+
     render () {
         if (this.props.isLoading) {
             return null;
@@ -9,7 +14,3 @@ export default class LoadingIndicator extends React.Component {
         return this.props.children;
     }
 }
-
-LoadingIndicator.propTypes = {
-    isLoading: PropTypes.bool.isRequired
-};
